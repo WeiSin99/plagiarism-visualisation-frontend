d54fcaee5b5fa85b7b8d0200b435e1a93ad0a335
WeiSin99 <looweisin008@gmail.com>
2022-07-11 22:23:55 +0100

    network visualisation

---
+++ src/components/Chart.js
@@ -0,0 +1,90 @@
+import { useEffect, useState, useRef } from 'react';
+import { scaleSequential } from 'd3-scale';
+import { interpolateRdYlBu } from 'd3-scale-chromatic';
+import {
+  forceSimulation,
+  forceLink,
+  forceCollide,
+  forceCenter,
+  forceManyBody,
+} from 'd3-force';
+
+const Chart = ({ report }) => {
+  const containerRef = useRef(null);
+  const [width, setWidth] = useState(0);
+  const [animatedNodes, setAnimatedNodes] = useState([]);
+
+  // set width on resizing
+  useEffect(() => {
+    setWidth(containerRef.current.clientWidth);
+
+    let timeoutId;
+    const resizeListener = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setWidth(containerRef.current.clientWidth);
+      }, 300);
+    };
+    window.addEventListener('resize', resizeListener);
+
+    return () => window.removeEventListener('resize', resizeListener);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // force simulation
+  useEffect(() => {
+    const colorScale = scaleSequential()
+      .domain([1, 0])
+      .interpolator(interpolateRdYlBu);
+
+    const nodes = report.potential_sources.map((source, i) => {
+      return {
+        id: source,
+        color: colorScale(report.scores[i]),
+      };
+    });
+    nodes.push({ id: 0, color: 'black' });
+
+    const links = report.potential_sources.map((source, i) => {
+      return {
+        source: 0,
+        target: source,
+        score: report.scores[i],
+      };
+    });
+
+    const simulation = forceSimulation(nodes)
+      .force(
+        'link',
+        forceLink(links)
+          .id((d) => d.id)
+          .strength((d) => d.score)
+      )
+      .force('center', forceCenter(width / 2, 250))
+      .force('collide', forceCollide(11))
+      .force('charge', forceManyBody().strength(-150));
+
+    simulation.on('tick', () => {
+      setAnimatedNodes([...simulation.nodes()]);
+    });
+
+    return () => simulation.stop();
+  }, [width]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div ref={containerRef} className="w-full">
+      <svg width="100%" height={500}>
+        {animatedNodes.map((node) => (
+          <circle
+            key={node.id}
+            cx={node.x}
+            cy={node.y}
+            r={10}
+            fill={node.color}
+          />
+        ))}
+      </svg>
+    </div>
+  );
+};
+
+export default Chart;
